2d4aa8608ef941d87a6fd69ec5c19b1da5de39d4
GMX\0B9066649 <sumalw@ibm.com>
2023-01-12 15:54:03 -0700

    DB Update

---
+++ NZWalks/NZWalksAPI/Migrations/20230112225110_InitialMigration2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalksAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WalkDifficultId",
+                table: "Walks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "WalkDifficultId",
+                table: "Walks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
